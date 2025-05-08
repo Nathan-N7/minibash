1e3fd9ca5361046a50630a9b6a776cc31017bf92
Nathan-N7 <nathan.rod1710@gmail.com>
2025-05-08 16:09:39 -0300

    norme 99%

--- libs/minishell.h
+++ libs/minishell.h
@@ -1,28 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: natrodri <natrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 16:07:55 by natrodri          #+#    #+#             */
+/*   Updated: 2025/05/08 16:09:11 by natrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # define MAX_ARGS 512
 # define MAX_REDIRS 32
 
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <stdlib.h>
-#include <signal.h>
-#include "structs.h"
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdlib.h>
+# include <signal.h>
+# include "structs.h"
 
-t_command    *parsing(char *input, char **envp);
-t_token	*tokenize(char *input);
-int	ft_isspace(char c);
-char *strip_aspas(char *str);
-void	free_tokens(t_token *head);
-int verify_aspas(char   *r);
-t_command   *new_command(t_command  **head);
-int    print_error(char *msg);
-int handle_word(t_command *cmd, t_token *tok, int *c, char **envp);
-int handle_redir(t_command *cmd, t_token **tok);
-int handle_pipe(t_command **cmd, int *count);
-void    free_commands(t_command *cmd);
-char	*get_value(char *name, char **envp);
-char    *expand_var(char *v, char **envp);
+t_command	*parsing(char *input, char **envp);
+t_token		*tokenize(char *input);
+int			ft_isspace(char c);
+char		*strip_aspas(char *str);
+void		free_tokens(t_token *head);
+int			verify_aspas(char *r);
+t_command	*new_command(t_command **head);
+int			print_error(char *msg);
+int			handle_word(t_command *cmd, t_token *tok, int *c, char **envp);
+int			handle_redir(t_command *cmd, t_token **tok);
+int			handle_pipe(t_command **cmd, int *count);
+void		free_commands(t_command *cmd);
+char		*get_value(char *name, char **envp);
+char		*expand_var(char *v, char **envp);
 
 #endif
--- libs/structs.h
+++ libs/structs.h
@@ -1,37 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: natrodri <natrodri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 15:55:43 by natrodri          #+#    #+#             */
+/*   Updated: 2025/05/08 15:58:07 by natrodri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
-typedef enum    s_tk_type
+typedef enum s_tk_type
 {
-    WORD,
-    PIPE,
-    REDIR_IN,  //<
-    REDIR_OUT, //>
-    APPEND,  //>>
-    HEREDOC //<<
-}   t_tk_type;
+	WORD,
+	PIPE,
+	REDIR_IN,
+	REDIR_OUT,
+	APPEND,
+	HEREDOC
+}	t_tk_type;
 
 typedef struct s_redirect
 {
-    t_tk_type type;
-    char    *filename;
-}   t_redirect;
+	t_tk_type	type;
+	char		*filename;
+}	t_redirect;
 
 typedef struct s_command
 {
-    char        **args;
-    t_redirect   *redirects;
-    int         redirect_count;
-    struct s_command    *next;
-}   t_command;
+	char				**args;
+	t_redirect			*redirects;
+	int					redirect_count;
+	struct s_command	*next;
+}	t_command;
 
 typedef struct s_indexvar
 {
-    int i;
-    int k;
-    int j;
-    int m;
-}   t_indexvar;
+	int	i;
+	int	k;
+	int	j;
+	int	m;
+}	t_indexvar;
 
 typedef struct s_expand
 {
@@ -42,11 +54,11 @@ typedef struct s_expand
 	char		*varname;
 }	t_expand;
 
-typedef struct  s_token
+typedef struct s_token
 {
-    t_tk_type       type;
-    char            *value;
-    struct s_token  *next;
-}   t_token;
+	t_tk_type		type;
+	char			*value;
+	struct s_token	*next;
+}	t_token;
 
 #endif
