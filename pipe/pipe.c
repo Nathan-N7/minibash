22d9db956d63c6ff8e92f38dc608f7d1de7eb85c
Nathan-N7 <nathan.rod1710@gmail.com>
2025-05-17 19:09:26 -0300

    norme 80%

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -2,57 +2,57 @@
 #include "../my_lib/libft.h"
 #include "../libs/structs.h"
 
-int is_builtin(t_command *cmd)
+int	is_builtin(t_command *cmd)
 {
-    if (!cmd || !cmd->args || !cmd->args[0])
-        return (0);
-    if (ft_strcmp(cmd->args[0], "cd") == 0
-        || ft_strcmp(cmd->args[0], "exit") == 0
-        || ft_strcmp(cmd->args[0], "pwd") == 0
-        || ft_strcmp(cmd->args[0], "env") == 0
-        || ft_strcmp(cmd->args[0], "unset") == 0
-        || ft_strcmp(cmd->args[0], "export") == 0
-        || ft_strcmp(cmd->args[0], "echo") == 0)
-        return (1);
-    return (0);
+	if (!cmd || !cmd->args || !cmd->args[0])
+		return (0);
+	if (ft_strcmp(cmd->args[0], "cd") == 0
+		|| ft_strcmp(cmd->args[0], "exit") == 0
+		|| ft_strcmp(cmd->args[0], "pwd") == 0
+		|| ft_strcmp(cmd->args[0], "env") == 0
+		|| ft_strcmp(cmd->args[0], "unset") == 0
+		|| ft_strcmp(cmd->args[0], "export") == 0
+		|| ft_strcmp(cmd->args[0], "echo") == 0)
+		return (1);
+	return (0);
 }
 
-void execute_cmd(t_command *cmd, char **envp)
+void	execute_cmd(t_command *cmd, char **envp)
 {
-    char *expand;
-    char **path;
-    int i;
-    char *join;
-    char *tmp;
+	char	*expand;
+	char	**path;
+	int	i;
+	char	*join;
+	char	*tmp;
 
-    i = 0;
-    expand = expand_var("$PATH", envp);
-    path = ft_split(expand, ':');
-    while (path[i])
-    {
-        join = ft_strjoin(path[i], "/");
-        tmp = join;
-        join = ft_strjoin(join, cmd->args[0]);
-        free(tmp);
-        if (access(join, F_OK) == 0)
-        {
-            execve(join, cmd->args, envp);
-            perror("execve");
-            free(join);
-            exit(1);
-        }
-        free(join);
-        i++;
-    }
+	i = 0;
+	expand = expand_var("$PATH", envp);
+	path = ft_split(expand, ':');
+	while (path[i])
+	{
+		join = ft_strjoin(path[i], "/");
+		tmp = join;
+		join = ft_strjoin(join, cmd->args[0]);
+		free(tmp);
+		if (access(join, F_OK) == 0)
+		{
+			execve(join, cmd->args, envp);
+			perror("execve");
+			free(join);
+			exit(1);
+		}
+		free(join);
+		i++;
+	}
 	printf("%s: command not found\n", cmd->args[0]);
 	exit (0);
 }
 
-void my_pipe(t_command *cmd, char **envp)
+void	my_pipe(t_command *cmd, char **envp)
 {
 	int		fd[2];
 	int		in_fd;
-	pid_t	pid;
+	pid_t		pid;
 
 	in_fd = 0;
 	while (cmd)
